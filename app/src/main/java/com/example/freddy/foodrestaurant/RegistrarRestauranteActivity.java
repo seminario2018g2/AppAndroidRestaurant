aacf5a0c7a9f5e4f79b216e54a6cfca4171e7f97
freddy <copamamanifreddy@gmail.com>
2018-12-08 15:52:42 -0400

    formulario_registrar_restaurant

--- app/src/main/java/com/example/freddy/foodrestaurant/RegistrarRestauranteActivity.java
+++ app/src/main/java/com/example/freddy/foodrestaurant/RegistrarRestauranteActivity.java
@@ -1,7 +1,13 @@
 package com.example.freddy.foodrestaurant;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,10 +18,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class RegistrarRestauranteActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView map;
     private GoogleMap mMap;
+    private Geocoder geocoder;
+    private TextView street;
+    private Button next;
+    private LatLng mainposition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,16 @@ public class RegistrarRestauranteActivity extends AppCompatActivity implements O
         map.onResume();
         MapsInitializer.initialize(this);
         map.getMapAsync(this);
+        geocoder = new Geocoder(getBaseContext(), Locale.getDefault());
+        street = findViewById(R.id.street);
+        next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //senData();
+            }
+        });
     }
 
     @Override
@@ -34,10 +59,13 @@ public class RegistrarRestauranteActivity extends AppCompatActivity implements O
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-19.5783329, -65.7563853);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Lugar").zIndex(17).draggable(true));
+        //-19.5783329,-65.7563853
+        LatLng potosi = new LatLng(-19.5783329, -65.7563853);
+        mainposition = potosi;
+        mMap.addMarker(new MarkerOptions().position(potosi).title("Lugar").zIndex(17).draggable(true));
         mMap.setMinZoomPreference(16);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(potosi));
+
         mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
             @Override
             public void onMarkerDragStart(Marker marker) {
@@ -51,9 +79,27 @@ public class RegistrarRestauranteActivity extends AppCompatActivity implements O
 
             @Override
             public void onMarkerDragEnd(Marker marker) {
-
+                mainposition = marker.getPosition();
+                String street_string = getStreet(marker.getPosition().latitude, marker.getPosition().longitude);
+                street.setText(street_string);
             }
         });
 
     }
+
+    public String getStreet (Double lat, Double lon) {
+        List<Address> address;
+        String result = "";
+        try {
+            address = geocoder.getFromLocation(lat, lon, 1);
+            result += address.get(0).getThoroughfare();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
